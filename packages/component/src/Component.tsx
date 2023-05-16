06e424f3f4d55c0aeb01d48293c0748539a98a41
Juan Carlos Rengifo Amaro <jcrengifoa@gmail.com>
2023-05-16 22:48:44 +0200

    Fix className for component package to show it with the proper color

--- packages/component/src/Component.tsx
+++ packages/component/src/Component.tsx
@@ -3,7 +3,7 @@ import { IComponentProps } from './component.types';
 import componentStyles from './component.module.scss';
 
 function Component(props: IComponentProps) {
-  return <p className={componentStyles.card}>{props.text}</p>;
+  return <p className={componentStyles.component}>{props.text}</p>;
 }
 
 export default Component;
